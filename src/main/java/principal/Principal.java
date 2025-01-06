b7bbddf50da794de24c4da6d4aae3748063c46c4
Mateus Victorio <mateus19992008@hotmail.com>
2025-01-06 19:58:44 -0300

    Updated project with Date validators and get episodes

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -1,14 +1,19 @@
 package principal;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
+import br.com.alura.screenmatch.model.Episodio;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 
@@ -53,7 +58,7 @@ public class Principal {
 		
 		//The "Stream" statement allow to use a lot of others functions.
 		//In the example below the follow code only order the list and print it.
-		nomes.stream().sorted().forEach(System.out::println);
+		/*nomes.stream().sorted().forEach(System.out::println);
 		
 		//In this example, it is ordering and showing the first three items of list.
 		nomes.stream().sorted().limit(3).forEach(System.out::println);
@@ -62,7 +67,42 @@ public class Principal {
 			 .sorted() //"sorted" to order the list
 			 .limit(3) //"limit" to show only three values 
 			 .filter(n -> n.startsWith("N")) //"filter" to do any logic in the lambda expression, in this case to show only names starts with "N"
-			 .map(n -> n.toUpperCase()) //"map" to change the value
-			 .forEach(System.out::println); //"forEach" to execute the print to each item on the list.
+			 .map(n -> n.toUpperCase()) //"map" to transform the value
+			 .forEach(System.out::println); //"forEach" to execute the print to each item on the list.*/
+		
+		
+		List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+				.flatMap(t -> t.episodios().stream())
+				.collect(Collectors.toList());
+		
+		System.out.println("\nTop 5 Episódios");
+		dadosEpisodios.stream()
+					.filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+					.sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+					.limit(5)
+					.forEach(System.out::println);
+		
+		
+		List<Episodio> episodios = temporadas.stream()
+				.flatMap(t -> t.episodios().stream()
+						.map(d -> new Episodio(t.numero(), d))
+						).collect(Collectors.toList());
+		
+		episodios.forEach(System.out::println);
+		
+		System.out.println("A partir de que ano você deseja ver os episódios? ");
+		var ano = leitura.nextInt();
+		leitura.nextLine();
+		
+		LocalDate dataBusca = LocalDate.of(ano, 1, 1);
+		
+
+		DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		episodios.stream()
+			.filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca))
+			.forEach(e -> System.out.println("Temporada: " + e.getTemporada() +
+												" Episódio: " + e.getTitulo() +
+												" Data lançamento: " + e.getDataLancamento().format(formatador)
+												));
 	}
 }
